3534e247a19d84e0b47a101cedee50f9e23e73b1
jirgens <jessa.irg@gmail.com>
2017-11-10 16:58:47 -0800

    Wrote a handler to confirm guests

--- src/App.js
+++ src/App.js
@@ -19,7 +19,20 @@ class App extends Component {
         isConfirmed: false
       }
     ]
-  }
+  };
+
+  toggleConfirmationAt = indexToChange =>
+    this.setState({
+      guests: this.state.guests.map((guest, index) => {
+        if (index === indexToChange) {
+          return {
+            ...guest,
+            isConfirmed: !guest.isConfirmed
+          }
+        }
+        return guest;
+      })
+    });
 
   getTotalInvited = () => this.state.guests.length;
   //getAttendingGuests = () =>
